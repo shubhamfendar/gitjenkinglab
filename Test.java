5230393323ccaed07b0bbeb6f1777e3ef1743236
shubhamfendar <fendar.shubham96@gmail.com>
2022-05-02 23:49:17 +0530

    first file added

---
+++ Test.java
@@ -0,0 +1,8 @@
+class test{ //test.class
+   public static void main(String ar[])
+   { System.out.print("hello\n");//hello
+	System.out.print("hello1\n");
+	System.out.println("hello2");
+	System.out.println("hello3");
+}
+}
